a3b943b3fea443483277b9b84aae814aa536f8e5
elias-291356 <ikucher.002@gmail.com>
2025-05-23 18:03:29 +0200

    login regist mutations
    4.23 mn  connect with backend

---
+++ src/features/auth/hooks/useRegisterMutation.tsx
@@ -0,0 +1,27 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { TypeRegisterSchema } from "../schemes";
+import { authService } from "../services";
+import { toastMessageHandler } from "@/shared/styles/utils";
+import { toast } from "sonner";
+
+export function useRegisterMutation() {
+  const { mutate: register, isPending: isLoadingRegister } = useMutation({
+    mutationKey: ["register user"],
+    mutationFn: ({
+      values,
+      recaptcha,
+    }: {
+      values: TypeRegisterSchema;
+      recaptcha: string;
+    }) => authService.register(values, recaptcha),
+    onSuccess(data: any) {
+      toastMessageHandler(data);
+    },
+    onError(error) {
+      toastMessageHandler(error);
+    },
+  });
+
+  return { register, isLoadingRegister };
+}
