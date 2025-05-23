6e1d62250b6dd74cc2de9d2b10bc14771bd49ac3
elias-291356 <ikucher.002@gmail.com>
2025-05-23 14:03:45 +0200

    register form

--- src/features/auth/components/AuthSocial.tsx
+++ src/features/auth/components/AuthSocial.tsx
@@ -2,25 +2,18 @@
 
 import { Button } from "@/shared/styles/components/ui";
 
-import { FaGoogle, FaYandex } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
 export function AuthSocial() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-6">
+          <div className="flex justify-center">
         <Button>
           <FaGoogle className="mr-2 size-4" />
           Google
         </Button>
       </div>
-      <div className="relative mb-2 space-y-4">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">Или</span>
-        </div>
-      </div>
+      
     </>
   );
 }
