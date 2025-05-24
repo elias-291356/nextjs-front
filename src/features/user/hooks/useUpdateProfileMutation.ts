35c31994c8b778b96517d7f6e0ec7d7f0c59fdd7
elias-291356 <ikucher.002@gmail.com>
2025-05-24 22:28:27 +0200

    show user profile logout button 5.21 and 2FA switch

---
+++ src/features/user/hooks/useUpdateProfileMutation.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner'
+import { userService } from '../services'
+import { TypeSettingsSchema } from '../schemas'
+import { toastMessageHandler } from '@/shared/styles/utils'
+
+export function useUpdateProfileMutation() {
+	const { mutate: update, isPending: isLoadingUpdate } = useMutation({
+		mutationKey: ['update profile'],
+		mutationFn: (data: TypeSettingsSchema) =>
+			userService.updateProfile(data),
+		onSuccess() {
+			toast.success('Профиль успешно обновлён')
+		},
+		onError(error) {
+			toastMessageHandler(error)
+		}
+	})
+
+	return { update, isLoadingUpdate }
+}
